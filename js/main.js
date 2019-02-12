0bbe4eb0cf5105a4bd810b928cca4f6d3c18b005
liamhendricks97 <mynameisliamhendricks@gmail.com>
2019-02-12 11:12:37 +0200

    intial commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+// 'use strict';
+console.log("Hello JavaScript");
+
+// let name="Liam";
+// console.log(name);
+
+// // constant
+// name="Liam";
+
+let name=" Liam";
+    surname=" Hendricks";
+    age=" 21";
+    sex=" male";
+    message=" Good morning";
+     
+    cake=" chocolate";
+    layer=" 3";
+    price=" R0.50";
+
+
+console.log(name);
+console.log(surname);
+console.log(age);
+console.log(sex);
+// alert(message + name + surname);
+// prompt(message)
+// alert("Hello Mr" + name + surname + "." + " We've heard you like Valentines cake and would like to sell you" + cake + " cake with" + layer + " layers at" + price + " each."); 
+console.log("Hello Mr" + name + surname + "." + " We've heard you like Valentines cake and would like to sell you" + cake + " cake with" + layer + " layers at" + price + " each.");
+
+// prompt name then name opens up,
+//  location,
+//   gift, 
+//   thank you for blah blah and enjoy valetine. 
+//   all in console.
+//   need 5 variables
