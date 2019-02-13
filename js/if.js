ca555d67e76e4d594240099371ee71ed4176d0e7
liamhendricks97 <mynameisliamhendricks@gmail.com>
2019-02-13 09:53:46 +0200

    added if statements

---
+++ js/if.js
@@ -0,0 +1,10 @@
+let number = prompt("Type a number.");
+    if (number >= "0") {
+        console.log (number + " is positive.")
+    }
+    else {
+        console.log (number + " is negative")
+    }
+
+    //     ||
+
--- js/main.js
+++ js/main.js
@@ -34,19 +34,35 @@
 //   all in console.
 //   need 5 variables.
 
-let place = " Signal Hill";
-    gift = " Flowers";
-    secondGift = " ChocoLate";
-    time = " 8pm";
-let message = prompt(" What is your name?");
-        switch (message) {
-            case message:
-                console.log(message + ", You're been invited to have a night out with yours truly by" + place + " at" + time + ". I'm looking forward to you and I hope you like" + gift + " and" + secondGift + ".")
+// let place = " Signal Hill";
+//     gift = " Flowers";
+//     secondGift = " ChocoLate";
+//     time = " 8pm";
+// let message = prompt(" What is your name?");
+//         switch (message) {
+//             case message:
+//                 console.log(message + ", You're been invited to have a night out with yours truly by" + place + " at" + time + ". I'm looking forward to you and I hope you like" + gift + " and" + secondGift + ".")
                 
-                break;
+//                 break;
         
-            default:
-                break;
-        }
+//             default:
+//                 break;
+//         }
+
+let     time = " 9pm";
+        gift = " Chocolate";
+        giftSecondary = " Flowers";
+
+alert("Welcome!");
+
+let name = prompt(" What is your name?");
+console.log("Hey, " + name);
+
+let placeX = prompt(" Where do you want to go?");
+console.log("You have chosen to go to " + placeX);
+
+console.log(name +  " you've been invited to have a night out with yours truly by " + placeX + " at" + time + ". I'm looking forward to you and I hope you like" + gift + " and" + giftSecondary +"." );
+alert(name + ", your Valentines is gonna be lit.");
+
 
 
